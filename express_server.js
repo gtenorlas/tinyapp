483120c8b54e488fd4563bf65fe00d480df2cc8a
Gene Tenorlas <tenorlas@sheridancollege.ca>
2022-08-25 13:57:19 +0000

    Create new view urls_new to show form

--- express_server.js
+++ express_server.js
@@ -3,11 +3,14 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true })); //to make the body in the POST request readable
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -36,11 +39,31 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+Instruction
+
+
+/*
+  NOTE: 
+  ! The GET /urls/new route needs to be defined before the GET /urls/:id route. Routes defined earlier will take precedence,
+  so if we place this route after the /urls/:id definition, any calls to /urls/new will be handled by app.get("/urls/:id", ...) 
+  because Express will think that new is a route parameter. A good rule of thumb to follow is that routes should be ordered
+  from most specific to least specific.
+*/
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const templateVars = { id, longURL: urlDatabase[id] };
   res.render("urls_show", templateVars);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
