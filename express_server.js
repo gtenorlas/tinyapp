861a9dd5a57c020393ae7ff663eeb7f0886a3b5e
Gene Tenorlas <tenorlas@sheridancollege.ca>
2022-08-30 05:54:02 +0000

    Added keys visitedCount, uniqueVisit, visitLogs in urlDatabase

--- express_server.js
+++ express_server.js
@@ -33,14 +33,23 @@ const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
     userID: "aJ48lW",
+    visitedCount: 0,
+    uniqueVisit: 0,
+    visitLogs: []
   },
   i3BoGr: {
     longURL: "https://www.google.ca",
     userID: "aJ48lW",
+    visitedCount: 3,
+    uniqueVisit: 2,
+    visitLogs: []
   },
   i34oGr: {
     longURL: "https://www.google.ca",
     userID: "abcdef",
+    visitedCount: 0,
+    uniqueVisit: 0,
+    visitLogs: []
   },
 };
 
